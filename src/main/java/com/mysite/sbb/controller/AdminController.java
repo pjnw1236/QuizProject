748c676310f2f2dbbc614c3f097f46cf867b87d6
pjnw1236 <pjnw1236@gmail.com>
2023-05-07 17:46:03 +0900

    feat: 관리자 계정 전용 Controller 생성

---
+++ src/main/java/com/mysite/sbb/controller/AdminController.java
@@ -0,0 +1,21 @@
+package com.mysite.sbb.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
+public class AdminController {
+
+
+    @GetMapping("/quiz")
+    public String getAdminQuiz() {
+        return "admin/admin_quiz";
+    }
+
+    @GetMapping("/board")
+    public String getAdminBoard() {
+        return "admin/admin_question_list";
+    }
+}
